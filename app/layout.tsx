810e38a3de1ff7b2bd62eff2399a86b9e5b7f5aa
Jul Guga <58825526+Mr-Vipi@users.noreply.github.com>
2024-11-27 17:32:24 +0100

    feat: add them light and dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next"
 import localFont from "next/font/local"
 
+import { ThemeProvider } from "@/components/theme-provider"
+
 import "./globals.css"
 
 const geistSans = localFont({
@@ -29,7 +31,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
