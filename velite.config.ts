51e9e16354ba7172e30b1bf7618cab9008f10889
Jul Guga <58825526+Mr-Vipi@users.noreply.github.com>
2024-12-02 18:19:06 +0100

    build: add velite package and configuration for blog management

---
+++ velite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, s } from "velite"
+
+// `s` is extended from Zod with some custom schemas,
+// you can also import re-exported `z` from `velite` if you don't need these extension schemas.
+
+export default defineConfig({
+  output: {
+    clean: true,
+  },
+  collections: {
+    posts: {
+      name: "Post", // collection type name
+      pattern: "blog/**/*.mdx", // content files glob pattern
+      schema: s
+        .object({
+          title: s.string().max(99), // Zod primitive type
+          // slug: s.slug("posts"), // validate format, unique in posts collection
+          slug: s.path(), // auto generate slug from file path
+          date: s.isodate(), // input Date-like string, output ISO Date string.
+          // cover: s.image(), // input image relative path, output image object with blurImage.
+          // video: s.file().optional(), // input file relative path, output file public path.
+          // metadata: s.metadata(), // extract markdown reading-time, word-count, etc.
+          // excerpt: s.excerpt(), // excerpt of markdown content
+          // content: s.markdown(), // transform markdown to html
+          description: s.string().max(999).optional(),
+          published: s.boolean().default(true),
+          body: s.mdx(),
+        })
+        // more additional fields (computed fields)
+        .transform((data) => ({
+          ...data,
+          slugAsParams: data.slug.split("/").slice(1).join("/"),
+        })),
+    },
+    // others: {
+    //   // other collection schema options
+    // },
+  },
+  mdx: {
+    rehypePlugins: [],
+    remarkPlugins: [],
+  },
+})
