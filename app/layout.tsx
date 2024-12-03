4235741ef04f86190f96a10b981f39bfb1d83d56
Jul Guga <58825526+Mr-Vipi@users.noreply.github.com>
2024-12-03 15:55:17 +0100

    fix: custom background pattern position

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,10 @@ export default function RootLayout({
         )}
       >
         <Providers>
-          <div className="relative flex min-h-dvh flex-col bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+          <div className="fixed left-0 top-0 -z-10 size-full">
+            <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_0%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)] dark:bg-neutral-950 dark:bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]" />
+          </div>
+          <div className="relative flex min-h-dvh flex-col">
             <SiteHeader />
             <main className="flex-1">{children}</main>
           </div>
