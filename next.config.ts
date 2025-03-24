06583b7b47152769038dea6eb81a92d01aeb86d4
Jul Guga <58825526+Mr-Vipi@users.noreply.github.com>
2025-03-24 16:25:35 +0100

    Revert "fix: test"
    This reverts commit 81c25762b569c0bddccf4c72dff80db3cd0e383c.

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,8 @@ import type { NextConfig } from "next"
 
 const isDev = process.argv.includes("dev")
 const isBuild = process.argv.includes("build")
-console.warn("proccess.argv", process.argv)
-console.warn("Velite_started", process.env.VELITE_STARTED)
 if (!process.env.VELITE_STARTED && (isDev || isBuild)) {
   process.env.VELITE_STARTED = "1"
-  console.warn("isDev", isDev)
-  console.warn("isBuild", isBuild)
   import("velite").then((v) => v.build({ watch: isDev, clean: !isDev }))
 }
 
